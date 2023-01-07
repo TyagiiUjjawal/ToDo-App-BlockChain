b5cd6d94811f425227cd6ccb9b1e7a984a326527
Ujjawal_Tyagi <tyagiujjawal9@gmail.com>
2023-01-07 07:42:26 +0530

    toDo

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+import abi from '../util/Todo.json';
+
+export const GreetingsText1 = "Todo. On the blockchain.";
+export const GreetingsText2 = "Create and manage your Todos directly from your browser on the blockchain!";
+
+export const CreateTaskText1 = "Create a new task";
+
+export const contractAddress = '';
+export const contractABI = abi.abi;
